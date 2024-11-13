e421fc31c9fe5cd46a3fac4a9a3e14470458c7b8
Linnovel <linnovel_88@hotmail.com>
2024-11-12 22:52:13 -0400

    terminar patch endpoint

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,18 +1,82 @@
 import {Request, Response} from 'express'
-import {check, validationResult} from 'express-validator'
 import Product from '../models/Product.model'
 
+
+//funcionalidades de los endpoints  crear los productos
+export const getProducsts =  async (req: Request, res: Response) => {
+           try {
+            //para llamar todos los productos
+            const products = await Product.findAll({
+                attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
+            });
+            res.json({data: products})
+           } catch (error) {
+            console.log(error)
+           }
+}
+//CREATE PRODUCT MIDDLEWARE
 export const createProduct = async (req : Request, res : Response): Promise<void>=> {
-    await check('name').notEmpty().withMessage('Nombre del producto no puede estar vacio').run(req);
-    await check('price').isNumeric().withMessage('Valor no valido').notEmpty().withMessage('El precio no puede estar vacio').custom(value => value > 0).withMessage('Precio no valido').run(req);
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return ;
+    try {
+        const product = await Product.create(req.body);
+        res.json({ data: product })
+    } catch (error) {
+        console.log(error)
     }
-
-    const product = await Product.create(req.body);
-    res.json({ data: product });
 };
 
+//GET PRODUCT
+export const getProducstsById =  async (req: Request, res: Response): Promise<void> => {
+    try {
+    // console.log(req.params.id)
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+    if(!product){
+         res.status(404).json({error: 'Producto no encontrado'})
+    } else {
+        res.json({data : product})
+    }
+    } catch (error) {
+     console.log(error)
+    }
+}
+
+
+//Update product
+
+export const updateProduct = async (req: Request, res: Response): Promise<void> => {
+
+        // console.log(req.params.id)
+        const {id} = req.params
+        const product = await Product.findByPk(id)
+        if(!product){
+             res.status(404).json({error: 'Producto no encontrado'})
+        } else {
+            res.json({data : product})
+        }
+
+        //Actualizar
+        
+        await product.update(req.body)
+        await product.save()
+        console.log(req.body)
+
+}
+
+
+export const updateAvailability = async (req: Request, res: Response):Promise<void> => {
+
+    // console.log(req.params.id)
+    //conseguir primero el Id
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+    if(!product){
+         res.status(404).json({error: 'Producto no encontrado'})
+    } 
+
+    //Actualizar
+    product.availability = req.body.availability
+    await product.save()
+    console.log(product.dataValues)
+    res.json({data : product})
+
+}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,27 +1,49 @@
 import {Router} from 'express'
-import { createProduct } from '../handlers/product'
+import { createProduct, getProducsts, getProducstsById, updateAvailability, updateProduct } from '../handlers/product'
+import {body, param} from 'express-validator'
+import { handleInputErrors } from '../middlewares'
 
 
 //Routing
 const router = Router()
 
 //GET 
-router.get("/",(req, res) => {
-    res.json('DESDE GET')
-})
+router.get("/", getProducsts)
+
+router.get("/:id", 
+    param('id').isInt().withMessage('ID no es valido'),
+    handleInputErrors,
+    getProducstsById
+)
 
 //POST
-router.post("/", createProduct)
+router.post("/",
+    //validacion de los endpoint
+    body('name').notEmpty().withMessage('Nombre del producto no puede estar vacio'),
+    body('price').isNumeric().withMessage('Valor no valido').notEmpty().withMessage('El precio no puede estar vacio').custom(value => value > 0).withMessage('Precio no valido'),
+    handleInputErrors,
+    createProduct
+)
 
-//PUST
-router.put("/",(req, res) => {
-    res.json('DESDE PUT')
-})
+//PUT
+//Realiza modificaciones totales a la base de datos
+router.put("/:id",
+    //validacion de los endpoint
+    body('name').notEmpty().withMessage('Nombre del producto no puede estar vacio'),
+    body('price').isNumeric().withMessage('Valor no valido').notEmpty().withMessage('El precio no puede estar vacio').custom(value => value > 0).withMessage('Precio no valido'),
+    body('Availability').isBoolean().withMessage('Valor para disponibilidad no valido'),
+    handleInputErrors,
+    updateProduct
+)
+
+//diferencias entre put y patch
+/**
+ * ACTUALIZA O REEMPLAZA COMPLETAMENTE EL RECURSO EXISTENTE (DESTRUCTIVO) ACTUALIZA COMPLETAMENTE
+ * PATCH REEMPLAZA SOLO EL ELEMENTO QUE LE ESTOY ENVIANDO (MENOS DESTRUCTIVO) MODIFICA SOLO LO QUE LE ENVIEMOS
+*/
 
 //PATCH
-router.patch("/",(req, res) => {
-    res.json('DESDE PATCH')
-})
+router.patch("/", updateAvailability)
 
 //DELETE
 router.delete("/",(req, res) => {
